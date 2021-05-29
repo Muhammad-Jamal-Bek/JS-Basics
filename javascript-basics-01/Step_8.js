aacc9de960d7b7603d8c30dac3041ae96f49d852
Muhammad Jamal Bek <Muhammad-jamal-bek@hotmail.com>
2021-05-30 00:18:39 +0300

    Eighth step

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,11 @@
+function func(age){
+    if(parseInt(age)<=0){
+        alert("So you're not even born? xD");
+    }
+    else if (parseInt(age)>=18){
+        alert("You are over 18!! Here's a beer! :D");
+    }
+    else{
+        alert ("You're under 18!! Leave the bar at once!! ");
+    }
+}
