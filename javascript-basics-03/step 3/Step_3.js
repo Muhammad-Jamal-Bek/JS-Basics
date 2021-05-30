4b78a301a46f8bdcd177bc529df5528cd2abe8ea
Muhammad Jamal Bek <Muhammad-jamal-bek@hotmail.com>
2021-05-31 01:35:17 +0300

    Third step

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+function func(clicked,p){
+    if(clicked=="color green"){
+        p.style.color="green";
+    }
+    else if(clicked=="color red"){
+        p.style.color="red";
+    }
+    else if(clicked=="color blue"){
+        p.style.color="blue";
+    }
+}
